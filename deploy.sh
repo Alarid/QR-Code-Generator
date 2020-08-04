ae5b135131d3462095568d9ca30118e17dbd8a4d
= <=>
2020-08-04 20:17:22 +0200

    Travis integration

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Alaird/QR-Code-Generator.git master:gh-pages
+git push -f git@github.com:Alarid/QR-Code-Generator.git master:gh-pages
 
 cd -
